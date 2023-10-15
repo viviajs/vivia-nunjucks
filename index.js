212fbffc35ea24596335fcaf7355710af3a82863
Saurlax <saurlax@qq.com>
2023-10-15 14:07:06 +0800

    feat: add environment to support more functions

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const nunjucks = require('nunjucks')
 
 module.exports = config => {
+  const env = new nunjucks.Environment(new nunjucks.FileSystemLoader('template'))
+  env.addGlobal('Object', Object)
+  env.addGlobal('Array', Array)
+
   return context => {
     context.path = context.path.replace(/\.[^\.]+$/, '.html')
     context.content = context.content.toString()
-    context.content = nunjucks.render(context.template, context)
+    context.content = env.render(context.template, context)
   }
 }
